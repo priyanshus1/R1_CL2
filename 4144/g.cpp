2e57f8f4086b7acb37fd62beb2b043013a5491f9
prasadmarne <praddymarne@gmail.com>
2015-06-30 10:21:42 +0530

    Create g.cpp

---
+++ 4144/g.cpp
@@ -0,0 +1,123 @@
+/*
+ * g.cpp
+ *
+ *  Created on: Jun 30, 2015
+ *      Author: pict
+ */
+#include<iostream>
+#include <list>
+#include<fstream>
+
+using namespace std;
+
+// This class represents a directed graph using adjacency list representation
+class Graph
+{
+    int V;    // No. of vertices
+    list<int> *adj;    // Pointer to an array containing adjacency lists
+    void DFSUtil(int v, bool visited[]);  // A function used by DFS
+public:
+    Graph(int V);  // Constructor
+    void addEdge(int v, int w); // function to add an edge to graph
+    void BFS(int s);  // prints BFS traversal from a given source s
+    void DFS(int v);
+};
+
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w); // Add w to v’s list.
+}
+
+void Graph::BFS(int s)
+{
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for(int i = 0; i < V; i++)
+        visited[i] = false;
+
+    // Create a queue for BFS
+    list<int> queue;
+
+    // Mark the current node as visited and enqueue it
+    visited[s] = true;
+    queue.push_back(s);
+
+    // 'i' will be used to get all adjacent vertices of a vertex
+    list<int>::iterator i;
+
+    while(!queue.empty())
+    {
+        // Dequeue a vertex from queue and print it
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+
+        // Get all adjacent vertices of the dequeued vertex s
+        // If a adjacent has not been visited, then mark it visited
+        // and enqueue it
+        for(i = adj[s].begin(); i != adj[s].end(); ++i)
+        {
+            if(!visited[*i])
+            {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
+}
+void Graph::DFSUtil(int v, bool visited[])
+{
+    // Mark the current node as visited and print it
+    visited[v] = true;
+    cout << v << " ";
+
+    // Recur for all the vertices adjacent to this vertex
+    list<int>::iterator i;
+    for(i = adj[v].begin(); i != adj[v].end(); ++i)
+        if(!visited[*i])
+            DFSUtil(*i, visited);
+}
+
+// DFS traversal of the vertices reachable from v. It uses recursive DFSUtil()
+void Graph::DFS(int v)
+{
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for(int i = 0; i < V; i++)
+        visited[i] = false;
+
+    // Call the recursive helper function to print DFS traversal
+    DFSUtil(v, visited);
+}
+// Driver program to test methods of graph class
+int main()
+{
+    // Create a graph given in the above diagram
+	fstream file;
+	file.open("xyz.txt");
+	int nodes,edges;
+	file >>nodes>>edges;
+	 Graph g(nodes);
+	int s1,d1,v1;
+
+	for (int k=0;k<edges;k++)
+	{
+		file >>s1>>d1>>v1;
+		g.addEdge(s1,d1);
+		g.addEdge(d1,s1);
+
+	}
+    g.BFS(0);
+    cout<<"\n";
+    g.DFS(0);
+    return 0;
+}
+
+
+
